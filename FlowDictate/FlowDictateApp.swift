4bbf12bc71c754f395a1ec688267989e036ee992
Hank1210 <reachme1000@yahoo.de>
2026-08-16 17:37:03 +0200

    Initial Commit

---
+++ FlowDictate/FlowDictateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowDictateApp.swift
+//  FlowDictate
+//
+//  Created by Frank Euler on 16.08.26.
+//
+
+import SwiftUI
+
+@main
+struct FlowDictateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
